59b0c8a45fd089206ff3ddfe281584097334cfd9
Rokib Hasan Sagar <10854164+rokibhasansagar@users.noreply.github.com>
2018-06-23 19:18:09 +0600

    Revert "Update compress.bash"

--- compress.bash
+++ compress.bash
@@ -20,7 +20,7 @@ manifest=https://github.com/LineageOS/android.git
 
 # Manifest branch.
 # Example: cm-14.0, nougat, Lineage-15.1
-branch=Lineage-15.1
+branch=cm-13.0
 
 ### User Autentication ###
 # Specify here your GitHub Info
