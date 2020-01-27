fedadf7ad4f136b0e394d2f269233d59b4b06126
Rokib Hasan Sagar <rokibhasansagar2014@outlook.com>
2020-01-27 16:45:39 +0000

    12: AOKP::mm

--- compress.bash
+++ compress.bash
@@ -13,15 +13,15 @@
 
 # Name of the ROM, (The Git Org Name) without any Spaces.
 # Example: CyanogenMod, SlimRoms, AOSP, ResurrectionRemix, TWRP
-name="CesiumOS"
+name="AOKP"
 
 # Manifest link. https:// is mandatory.
 # Example: https://github.com/cyanogenmod/android.git
-manifest="https://github.com/CesiumOS/manifest.git"
+manifest="https://github.com/AOKP/platform_manifest.git"
 
 # Manifest branch.
 # Example: cm-14.0, nougat, Lineage-15.1
-branch="ten"
+branch="mm"
 
 ### Finally, execute the stuff. ###
 # Do Not Touch The Following Line
