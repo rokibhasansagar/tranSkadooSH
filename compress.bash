48775cd1bdc5bf2a119b83e6d2136654a744e44d
Rokib Hasan Sagar <rokibhasansagar2014@outlook.com>
2020-01-27 08:45:43 +0000

    9: DotOS::dot-n

--- compress.bash
+++ compress.bash
@@ -13,15 +13,15 @@
 
 # Name of the ROM, (The Git Org Name) without any Spaces.
 # Example: CyanogenMod, SlimRoms, AOSP, ResurrectionRemix, TWRP
-name="SlimRoms"
+name="DotOS"
 
 # Manifest link. https:// is mandatory.
 # Example: https://github.com/cyanogenmod/android.git
-manifest="https://github.com/SlimRoms/platform_manifest.git"
+manifest="https://github.com/DotOS/manifest.git"
 
 # Manifest branch.
 # Example: cm-14.0, nougat, Lineage-15.1
-branch="or8.1"
+branch="dot-n"
 
 ### Finally, execute the stuff. ###
 # Do Not Touch The Following Line
