6cf8e83478d24797b1e0e2f668d70ccdc3eff641
Rokib Hasan Sagar <rokibhasansagar2014@outlook.com>
2020-01-28 16:24:34 +0600

    18: AICP::mm6.0

--- compress.bash
+++ compress.bash
@@ -13,15 +13,15 @@
 
 # Name of the ROM, (The Git Org Name) without any Spaces.
 # Example: CyanogenMod, SlimRoms, AOSP, ResurrectionRemix, TWRP
-name="CarbonROM"
+name="AICP"
 
 # Manifest link. https:// is mandatory.
 # Example: https://github.com/cyanogenmod/android.git
-manifest="https://github.com/CarbonROM/android.git"
+manifest="https://github.com/AICP/platform_manifest.git"
 
 # Manifest branch.
 # Example: cm-14.0, nougat, Lineage-15.1
-branch="cr-6.1"
+branch="mm6.0"
 
 ### Finally, execute the stuff. ###
 # Do Not Touch The Following Line
