940a57b3b8809da5d2fb0cbf64372f5722c63cb0
Rokib Hasan Sagar <rokibhasansagar2014@outlook.com>
2020-02-02 17:31:18 +0000

    34: XenonHD::o (oreo)

--- compress.bash
+++ compress.bash
@@ -21,7 +21,7 @@ manifest="https://github.com/TeamHorizon/platform_manifest.git"
 
 # Manifest branch.
 # Example: cm-14.0, nougat, Lineage-15.1
-branch="n"
+branch="o"
 
 ### Finally, execute the stuff. ###
 # Do Not Touch The Following Line
