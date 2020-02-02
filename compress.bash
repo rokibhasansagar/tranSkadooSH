98c26c013f48450bc15c628d9e75124829d43878
Rokib Hasan Sagar <rokibhasansagar2014@outlook.com>
2020-02-02 19:32:04 +0600

    33: XenonHD::n (nougat)

--- compress.bash
+++ compress.bash
@@ -13,15 +13,15 @@
 
 # Name of the ROM, (The Git Org Name) without any Spaces.
 # Example: CyanogenMod, SlimRoms, AOSP, ResurrectionRemix, TWRP
-name="MoKee"
+name="XenonHD"
 
 # Manifest link. https:// is mandatory.
 # Example: https://github.com/cyanogenmod/android.git
-manifest="https://github.com/MoKee/android.git"
+manifest="https://github.com/TeamHorizon/platform_manifest.git"
 
 # Manifest branch.
 # Example: cm-14.0, nougat, Lineage-15.1
-branch="mkp"
+branch="n"
 
 ### Finally, execute the stuff. ###
 # Do Not Touch The Following Line
