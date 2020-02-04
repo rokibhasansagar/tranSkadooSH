02eae341ece9192297d8e5d0ae0a2981e2efbce1
Rokib Hasan Sagar <rokibhasansagar2014@outlook.com>
2020-02-04 17:33:24 +0000

    37: TeslaOS::8.1

--- compress.bash
+++ compress.bash
@@ -13,15 +13,15 @@
 
 # Name of the ROM, (The Git Org Name) without any Spaces.
 # Example: CyanogenMod, SlimRoms, AOSP, ResurrectionRemix, TWRP
-name="TeslaRom"
+name="TeslaOS"
 
 # Manifest link. https:// is mandatory.
 # Example: https://github.com/cyanogenmod/android.git
-manifest="https://github.com/TeslaRom-N/manifest.git"
+manifest="https://github.com/Tesla-OS/manifest.git"
 
 # Manifest branch.
 # Example: cm-14.0, nougat, Lineage-15.1
-branch="n7.1.2"
+branch="8.1"
 
 ### Finally, execute the stuff. ###
 # Do Not Touch The Following Line
